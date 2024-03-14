2eda1f9eb44d7748efe81e7289f99e843f78c1db
Mike Torres <michael@aqwire.co>
2024-03-14 09:17:05 +0700

    finish bom generator details section

--- src/pages/bom_generator_page.py
+++ src/pages/bom_generator_page.py
@@ -1,10 +1,11 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
 from components import bom_generator_details_section, bom_generator_requirements_section
+from constants.styles import PAGE_PADDING_STYLE
 
 
 def render(app: Dash) -> dbc.Row:
     return dbc.Row([
         dbc.Row(bom_generator_details_section.render(app)),
         dbc.Row(bom_generator_requirements_section.render(app))
-    ])
+    ], style=PAGE_PADDING_STYLE)
